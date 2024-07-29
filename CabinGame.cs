a9b5fe775094e60f28b7767c18dfa01a5f23eb79
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-07-29 10:07:58 +0400

    Kinda works

--- CabinGame.cs
+++ CabinGame.cs
@@ -5,6 +5,7 @@ public partial class CabinGame : Node3D
 {
 
 	private Camera3D camera3D;
+	private Node3D dummyCamera;
 	private CameraMarker initialMarker;
 	private CameraMarker knifeMarker;
 
@@ -14,6 +15,7 @@ public partial class CabinGame : Node3D
 	public override void _Ready()
 	{
 		camera3D = GetNode<Camera3D>("Camera3D");
+		dummyCamera = GetNode<Node3D>("DummyCamera");
 		initialMarker = GetNode<CameraMarker>("InitialMarker");
 		knifeMarker = GetNode<CameraMarker>("KnifeMarker");
 
@@ -43,8 +45,20 @@ public partial class CabinGame : Node3D
 
 		if (currentCameraMarker != null)
 		{
-			camera3D.GlobalPosition = currentCameraMarker.cameraMarker.GlobalPosition;
-			camera3D.LookAt(currentCameraMarker.lookAtMarker.GlobalPosition);
+			dummyCamera.GlobalPosition = currentCameraMarker.cameraMarker.GlobalPosition;
+			dummyCamera.LookAt(currentCameraMarker.lookAtMarker.GlobalPosition);
+			var rotationTween = CreateTween();
+			rotationTween.SetParallel(true);
+			rotationTween.TweenProperty(camera3D, new NodePath("global_position"), dummyCamera.GlobalPosition, 0.3f).SetTrans(Tween.TransitionType.Spring);
+			rotationTween.TweenProperty(camera3D, new NodePath("rotation"), dummyCamera.Rotation, 0.3f).SetTrans(Tween.TransitionType.Spring);
+
+			// rotationTween.TweenProperty(camera3D, new NodePath("global_position"), currentCameraMarker.cameraMarker.GlobalPosition, 0.3f).SetTrans(Tween.TransitionType.Spring);
+			// // rotationTween.TweenProperty(camera3D, new NodePath("transform"), camera3D.Transform.LookingAt(currentCameraMarker.lookAtMarker.GlobalPosition, new Vector3(0, 1, 0)), 0.3f).SetTrans(Tween.TransitionType.Spring);
+
+			// rotationTween.TweenProperty(camera3D, new NodePath("global_transform"), camera3D.GlobalTransform.LookingAt(currentCameraMarker.lookAtMarker.GlobalTransform.Origin, new Vector3(0, 1, 0)), 0.3f).SetTrans(Tween.TransitionType.Spring);
+
+			// // camera3D.GlobalPosition = currentCameraMarker.cameraMarker.GlobalPosition;
+			// // camera3D.LookAt(currentCameraMarker.lookAtMarker.GlobalPosition);
 		}
 	}
 }
