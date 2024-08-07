d8d81378291b9794ae7c760ac3888d2586e354b8
Daniil Shevtsov <daniil.shevtsov@aviasales.ru>
2024-08-07 22:10:09 +0400

    chalk sound works

--- CabinGame.cs
+++ CabinGame.cs
@@ -14,6 +14,10 @@ public partial class CabinGame : Node3D
 	private Node3D doorLight;
 	private AudioStreamPlayer scritchPlayer;
 
+	private AudioStreamPlayer actionPlayer;
+	private MeshInstance3D circle;
+	private ShaderMaterial circleMaterial;
+
 	private List<CameraMarker> markersToCycle = new();
 
 	private CameraMarker currentCameraMarker = null;
@@ -31,6 +35,11 @@ public partial class CabinGame : Node3D
 		doorLight = GetNode<Node3D>("DoorLight");
 		scritchPlayer = GetNode<AudioStreamPlayer>("ScritchAudioPlayer");
 
+		actionPlayer = GetNode<AudioStreamPlayer>("ActionAudioPlayer");
+		circle = (MeshInstance3D)FindChild("Circle");
+		circleMaterial = (ShaderMaterial)circle.GetSurfaceOverrideMaterial(0);
+		circleMaterial.SetShaderParameter("dissolve_value", 0f);
+
 		currentCameraMarker = initialMarker;
 		SwitchTo(initialMarker);
 
@@ -40,13 +49,28 @@ public partial class CabinGame : Node3D
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override async void _Process(double delta)
 	{
 		if (Input.IsActionJustReleased("moon"))
 		{
 			scritchPlayer.Play();
 			doorLight.Visible = !doorLight.Visible;
 		}
+
+		if (Input.IsActionJustReleased("chalk"))
+		{
+			var finalValue = 1f;
+			if ((float)circleMaterial.GetShaderParameter("dissolve_value") > 0f)
+			{
+				finalValue = 0f;
+			}
+			var tween = CreateTween();
+			tween.TweenProperty(circleMaterial, "shader_parameter/dissolve_value", finalValue, 1.0f);
+			actionPlayer.Play();
+			await ToSignal(tween, "finished");
+			actionPlayer.Stop();
+		}
+
 
 		if (Input.IsActionJustReleased("jump"))
 		{
@@ -59,14 +83,6 @@ public partial class CabinGame : Node3D
 			currentMarkerIndex = newIndex;
 
 			SwitchTo(markersToCycle[currentMarkerIndex]);
-			// if (currentCameraMarker == initialMarker)
-			// {
-			// 	SwitchTo(knifeMarker);
-			// }
-			// else if (currentCameraMarker == knifeMarker)
-			// {
-			// 	SwitchTo(initialMarker);
-			// }
 		}
 	}
 
